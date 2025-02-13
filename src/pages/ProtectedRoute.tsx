89a10369a6cd4978aee5dc1a5812ffdaad9c7c19
AungBoMyint <aungbomyint292@gmail.com>
2025-02-14 01:54:31 +0700

    fix account loading

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -3,20 +3,19 @@ import authStore from "@/hooks/authStore";
 import { useCurrentUser } from "@/hooks/useAuth";
 import { useEffect } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import AccountLoading from "./AccountLoading";
 
 const ProtectedRoute = () => {
   var localUser: AppUser | undefined = JSON.parse(
     localStorage.getItem("user") || "{}"
   );
-  const { isLoading, data } = useCurrentUser(localUser?.id ?? "");
+  const { data } = useCurrentUser(localUser?.id ?? "");
   const location = useLocation();
   useEffect(() => {
     authStore.getState().setUser!(data?.data() as AppUser);
   }, [data?.exists()]);
-  if (isLoading) {
+  /*  if (isLoading) {
     return <AccountLoading />;
-  }
+  } */
   return data?.exists() || localUser?.id ? (
     <Outlet />
   ) : (
