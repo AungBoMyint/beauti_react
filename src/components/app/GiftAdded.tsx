c33a2c253b97f9fe853b054f166bd83983ad36b7
AungBoMyint <aungbomyint292@gmail.com>
2025-02-19 18:56:45 +0700

    minimum spend limit to gift-item

--- src/components/app/GiftAdded.tsx
+++ src/components/app/GiftAdded.tsx
@@ -1,32 +1,143 @@
-import { ReactNode } from "react";
+import Confetti from "react-confetti";
 import {
   DialogBody,
   DialogContent,
   DialogRoot,
   DialogTrigger,
 } from "../ui/dialog";
-
-interface GiftProps {
-  trigger: ReactNode;
-  content: ReactNode;
+import birthdayGift from "../../assets/birthday.svg";
+import { Box, Button, Center, Image, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import useCart from "@/hooks/useCart";
+import authStore from "@/hooks/authStore";
+import GiftItem from "@/entity/GiftItem";
+import { useWindowSize } from "react-use";
+import { IoCheckmarkCircleSharp } from "react-icons/io5";
+import { useState } from "react";
+import { toaster } from "../ui/toaster";
+interface Props {
+  item: GiftItem;
 }
-
-const GiftAdded = ({ trigger, content }: GiftProps) => {
+const GiftAdded = ({ item }: Props) => {
+  const addItem = useCart.getState().addItem;
+  const setUser = authStore.getState().setUser;
+  const currentUser = authStore.getState().currentUser;
+  const grandTotal = useCart((state) => state.grandTotal);
+  const [open, setOpen] = useState(false);
   return (
     <DialogRoot
       role="alertdialog"
-      /*   open={open}
-      onOpenChange={(e) => setOpen(e.open)} */
+      open={open}
+      //onOpenChange={(e) => setOpen(e.open)}
       size="cover"
       placement="center"
       motionPreset="slide-in-bottom"
     >
-      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogTrigger asChild>
+        <Button
+          onClick={() => {
+            if (grandTotal < 50000) {
+              setOpen(false);
+              toaster.create({
+                title:
+                  "You need to spend at least 50,000 Ks to claim this gift.",
+                type: "error",
+              });
+              return;
+            }
+            setOpen(true);
+            addItem({
+              advertisementID: "",
+              brandID: "",
+              brandName: "",
+              category: [],
+              color: "",
+              comment: null,
+              dateTime: "",
+              deliveryTime: null,
+              description: item.desc,
+              discountPrice: null,
+              howToUse: null,
+              id: item.id,
+              ingredients: null,
+              love: null,
+              name: item.name,
+              originalPrice: 0,
+              originalQuantity: 0,
+              photo1: item.image,
+              photo2: null,
+              photo3: null,
+              price: 0,
+              remainQuantity: 0,
+              requirePoint: null,
+              reviewCount: null,
+              scheduleSale: null,
+              size: null,
+              status: null,
+              tags: null,
+              isGift: true,
+            });
+            setUser!({
+              ...currentUser!,
+              claimed: [
+                new Date().getFullYear().toString(),
+                ...(currentUser?.claimed ?? []),
+              ],
+            });
+          }}
+          width={"full"}
+          variant={"outline"}
+          border={"solid"}
+          borderColor={"gold"}
+          color={"gold"}
+          rounded={"lg"}
+          fontWeight={"medium"}
+          mt={2}
+        >
+          Claim
+        </Button>
+      </DialogTrigger>
       <DialogContent height={"fit"}>
-        <DialogBody>{content}</DialogBody>
+        <DialogBody>
+          <AddedCartConfetti />
+        </DialogBody>
       </DialogContent>
     </DialogRoot>
   );
 };
 
 export default GiftAdded;
+const AddedCartConfetti = () => {
+  const navigate = useNavigate();
+  const { width, height } = useWindowSize();
+  return (
+    <>
+      {" "}
+      <Confetti tweenDuration={100} width={width} height={height} />
+      <Box justifySelf={"center"}>
+        <Center>
+          <IoCheckmarkCircleSharp color="green" size={40} />
+        </Center>
+        <Center>
+          <Image src={birthdayGift} />
+        </Center>
+        <Text textAlign={"center"} fontWeight={"bold"} fontSize={"lg"} mb={1}>
+          Gift Added
+        </Text>
+        <Text fontSize={"md"}>Your gift has been added to your cart!</Text>
+        <Button
+          onClick={() => {
+            navigate("/cart");
+          }}
+          mt={2}
+          variant={"solid"}
+          color={"white"}
+          width={"full"}
+          bg={{ base: "black", _dark: "gray.800" }}
+        >
+          Go to cart
+        </Button>
+      </Box>
+    </>
+  );
+};
--- src/pages/BirthdayGiftProducts.tsx
+++ src/pages/BirthdayGiftProducts.tsx
@@ -1,24 +1,17 @@
-import { Box, Button, Center, Flex, Grid, Image, Text } from "@chakra-ui/react";
-import { useWindowSize } from "react-use";
-import Confetti from "react-confetti";
+import { Box, Flex, Grid, Image, Text } from "@chakra-ui/react";
+
 import gift from "../assets/gift.png";
 import GiftAdded from "@/components/app/GiftAdded";
-import birthdayGift from "../assets/birthday.svg";
-import { IoCheckmarkCircleSharp } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
-import useCart from "@/hooks/useCart";
+
 import { useGetBirthdayGifts } from "@/hooks/useBirthdayGift";
-import authStore from "@/hooks/authStore";
+import { Toaster } from "@/components/ui/toaster";
 
 const BirthdayGiftProducts = () => {
-  const navigate = useNavigate();
-  const { width, height } = useWindowSize();
-  const addItem = useCart.getState().addItem;
   const { isLoading, data: items } = useGetBirthdayGifts();
-  const setUser = authStore.getState().setUser;
-  const currentUser = authStore.getState().currentUser;
+
   return (
     <>
+      <Toaster />
       <Flex
         position={"fixed"}
         bg={"white"}
@@ -74,105 +67,7 @@ const BirthdayGiftProducts = () => {
                       <Text color={"gray"} fontSize={"sm"}>
                         {item.desc}
                       </Text>
-                      <GiftAdded
-                        trigger={
-                          <Button
-                            onClick={() => {
-                              addItem({
-                                advertisementID: "",
-                                brandID: "",
-                                brandName: "",
-                                category: [],
-                                color: "",
-                                comment: null,
-                                dateTime: "",
-                                deliveryTime: null,
-                                description: item.desc,
-                                discountPrice: null,
-                                howToUse: null,
-                                id: item.id,
-                                ingredients: null,
-                                love: null,
-                                name: item.name,
-                                originalPrice: 0,
-                                originalQuantity: 0,
-                                photo1: item.image,
-                                photo2: null,
-                                photo3: null,
-                                price: 0,
-                                remainQuantity: 0,
-                                requirePoint: null,
-                                reviewCount: null,
-                                scheduleSale: null,
-                                size: null,
-                                status: null,
-                                tags: null,
-                                isGift: true,
-                              });
-                              setUser!({
-                                ...currentUser!,
-                                claimed: [
-                                  new Date().getFullYear().toString(),
-                                  ...(currentUser?.claimed ?? []),
-                                ],
-                              });
-                            }}
-                            width={"full"}
-                            variant={"outline"}
-                            border={"solid"}
-                            borderColor={"gold"}
-                            color={"gold"}
-                            rounded={"lg"}
-                            fontWeight={"medium"}
-                            mt={2}
-                          >
-                            Claim
-                          </Button>
-                        }
-                        content={
-                          <>
-                            <Confetti
-                              tweenDuration={100}
-                              width={width}
-                              height={height}
-                            />
-                            <Box justifySelf={"center"}>
-                              <Center>
-                                <IoCheckmarkCircleSharp
-                                  color="green"
-                                  size={40}
-                                />
-                              </Center>
-                              <Center>
-                                <Image src={birthdayGift} />
-                              </Center>
-                              <Text
-                                textAlign={"center"}
-                                fontWeight={"bold"}
-                                fontSize={"lg"}
-                                mb={1}
-                              >
-                                Gift Added
-                              </Text>
-                              <Text fontSize={"md"}>
-                                Your gift has been added to your cart!
-                              </Text>
-                              <Button
-                                onClick={() => {
-                                  navigate("/cart");
-                                }}
-                                mt={2}
-                                variant={"solid"}
-                                color={"white"}
-                                width={"full"}
-                                bg={{ base: "black", _dark: "gray.800" }}
-                              >
-                                Go to cart
-                              </Button>
-                            </Box>
-                          </>
-                        }
-                      />
+                      <GiftAdded item={item} />
                     </Box>
                   </Flex>
                 );
